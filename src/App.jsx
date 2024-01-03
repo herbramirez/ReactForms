bfdadf085d3441e20bf288c52a769d5e701e1ebe
Herb <herb@Herbs-MBP.MG8702>
2024-01-03 11:02:55 -0800

    tab 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Authenticate from './Authenticate'
+import SignUpForm from './SignUpForm'
 
 function App() {
-
+<>
+  <SignUpForm/>
+  <Authenticate/>
+</>
 }
 
 export default App
---
+++ src/SignUpForm.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function SignUpForm() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState(null)
+
+
+    return
+    <>
+      <h2>Sign Up!</h2>
+      <form>
+        <label>
+         Username: <input />
+         </label>
+     <label>
+         Password: <input />
+     </label>
+     <button>Submit</button>
+</form>
+
+    </>
+  
+}
